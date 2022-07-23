b6051990bc2d2b5d3eff1e1f04c7fb76e1005996
AntonFeklichev <afeklichev@yandex.ru>
2022-07-23 16:10:16 +0400

    get-all-tasks endpoint added

---
+++ src/main/java/manager/http/handlers/TasksHandler.java
@@ -0,0 +1,39 @@
+package manager.http.handlers;
+
+import com.sun.net.httpserver.HttpExchange;
+import manager.TaskManager;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.Optional;
+
+public class TasksHandler extends AbstractHandler{
+    public TasksHandler(TaskManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String path = exchange.getRequestURI().getPath().toLowerCase();
+        String method = exchange.getRequestMethod();
+        String query = exchange.getRequestURI().getQuery();
+        Optional<Map<String, String>> queryMap = Optional.ofNullable(queryToMap(query));
+        logger.info("handling request\n" +
+                "method: " + method +
+                "\npath: " + path);
+        switch (method){
+            case "GET":
+                response = gson.toJson(manager.getTasksOfAllTypes());
+                rCode = 200;
+                break;
+        }
+        logger.info("response sent " +
+                "\nrCode: " + rCode);
+        exchange.sendResponseHeaders(rCode, responseLength);
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(response.getBytes(StandardCharsets.UTF_8));
+        }
+    }
+}
